9971afb9c2db1f69a47e9ed4f9dba5f0737a82be
Fatmatrn <ftmtrn3488@gmail.com>
2024-11-07 14:09:50 +0300

    f

--- src/components/parallax/index.jsx
+++ src/components/parallax/index.jsx
@@ -7,8 +7,9 @@ const Parallax = () => {
       <div className="absolute  bottom-0 left-0 w-0 h-0 border-l-[40px] sm:border-l-[60px] md:border-l-[80px] border-l-transparent border-t-[40px] sm:border-t-[60px] md:border-t-[80px] border-t-blue-500 border-r-[40px] sm:border-r-[60px] md:border-r-[80px] border-r-transparent rotate-45 opacity-70"></div>
 
       <p className="relative z-10 p-3 sm:p-5 bg-black bg-opacity-50 rounded-lg max-w-[90%] sm:max-w-[80%] md:max-w-[60%]">
-        "Understanding the past comes from understanding data. Predicting the future comes from using data."
-      </p>
+  {"\"Understanding the past comes from understanding data. Predicting the future comes from using data.\""}
+</p>
+
 
       <span className="absolute bottom-0 right-0 mr-5 mb-3 text-lg sm:text-xl md:text-2xl font-semibold">
       — Nate Silver
