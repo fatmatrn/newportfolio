91d15a3d4c5f14d835a9356a4aa1b3b345324e60
Fatmatrn <ftmtrn3488@gmail.com>
2024-11-08 20:51:13 +0300

    jj

--- src/components/parallax/index.jsx
+++ src/components/parallax/index.jsx
@@ -5,12 +5,12 @@ const Parallax = () => {
     <div className="relative bg-cover bg-center bg-no-repeat bg-fixed flex justify-center items-center p-5 sm:p-10 md:p-20 text-xl sm:text-2xl text-white font-bold h-[40vh] sm:h-[50vh] md:h-[60vh] bg-[url('/media/bgdata.jpeg')]">
       
       {/* Tüm sayfanın üstüne mavi bir filtre katmanı */}
-      <div className="absolute inset-0 bg-orange-950 opacity-30 z-0"></div>
+      <div className="absolute inset-0 bg-orange-950 opacity-35 z-0"></div>
 
       {/* Sol altta küçük, yarı saydam üçgen */}
       <div className="absolute bottom-0 left-0 w-0 h-0 border-l-[40px] sm:border-l-[60px] md:border-l-[80px] border-l-transparent border-t-[40px] sm:border-t-[60px] md:border-t-[80px] border-t-blue-700 border-r-[40px] sm:border-r-[60px] md:border-r-[80px] border-r-transparent rotate-45 opacity-70 z-10"></div>
 
-      <p className="relative z-10 p-3 sm:p-5 bg-blue-700 bg-opacity-35 text-white rounded-lg max-w-[90%] sm:max-w-[80%] md:max-w-[60%]">
+      <p className="relative z-10 p-3 sm:p-5 bg-blue-700 bg-opacity-40 text-white rounded-lg max-w-[90%] sm:max-w-[80%] md:max-w-[60%]">
         {"\"Understanding the past comes from understanding data. Predicting the future comes from using data.\""}
       </p>
 
